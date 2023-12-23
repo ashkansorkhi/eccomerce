cf7076ae9e6b1add86908cec78bb4f137dd57998
ashkan sorkhi <92922896+ashkansorkhi@users.noreply.github.com>
2023-12-23 10:11:54 +0330

    initialCommit

---
+++ eccomerce/src/components/Header.js
@@ -0,0 +1,10 @@
+import cetecIcon from "./pictures/2420d38cc351dfb8422cc19cf2924dfe.png";
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={cetecIcon} alt="cetecIcon" />
+    </header>
+  );
+}
